c84fd36ebfc8decf98d9f9c3f1f8d5b61a119509
Oskar <oskar0201@onet.pl>
2024-01-26 15:06:16 +0100

    app component with application structure and password options object

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import {Container} from "./components/Container/Container.jsx";
+import {Header} from "./components/Header/Header.jsx";
+import {Form} from "./components/Form/Form.jsx";
+import {useState} from "react";
+import {Footer} from "./components/Footer/Footer.jsx";
+
 export function App() {
+    const [isError, setIsError] = useState(false);
+    const [settings, setSettings] = useState({
+        passwordLength: 10,
+        includeLetters: true,
+        includeCapitalLetters: false,
+        includeNumbers: false,
+        includeSpecialChars: false,
+    });
 
   return (
-    <>
-
-    </>
+      <Container>
+          <Header/>
+          <Form settings={settings} setSettings={setSettings} setIsError={setIsError}/>
+          <Footer settings={settings} isError={isError}/>
+      </Container>
   )
 }
